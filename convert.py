781d7e2a5248e83cb6e3a8d99beb209459c79849
Redmoogle <dakotamew@gmail.com>
2022-05-06 17:19:15 -0400

    Points Only for Frackin Universe

--- convert.py
+++ convert.py
@@ -3,19 +3,24 @@ import json5 as json
 
 files = os.listdir("./zb/researchTree")
 for file in files:
-    with open(f"./zb/researchTree/{file}", 'r+') as config:
+    with open(f"./zb/researchTree/{file}", 'r') as config:
         jsonobj = json.load(config)
         researchTree = jsonobj["researchTree"]
         for catagory in researchTree:
             researchCatagory = catagory
             catagoryObj = researchTree[catagory] # Incase theres multiple catagories in one file
             for key in catagoryObj:
-                price = catagoryObj[key]["price"]
+                try:
+                    price = catagoryObj[key]["price"]
+                except KeyError: # Not a catagory
+                    break
                 try:
                     scicost = price = catagoryObj[key]["price"][0]
                     catagoryObj[key]["price"] = [scicost]
                 except IndexError:
                     pass # Some of them cost nothing
                 researchTree[catagory] = catagoryObj
+
+    with open(f"./zb/researchTree/{file}", 'w+') as config:
         jsonobj["researchTree"] = researchTree
-        json.dump(jsonobj, config)
+        json.dump(jsonobj, config, indent=4)
